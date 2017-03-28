83304fb64abbc66736827a0cdb12638dce338424
進撃の雯雯老師 <askahu6@gmail.com>
2017-03-28 17:15:48 -0500

    Update hclust.R

--- hclust.R
+++ hclust.R
@@ -4,13 +4,31 @@ rm(list=ls())
 #Server
 
 load('xdist.rda')
+#xhc <- hclust(xdist)
+
 cluster <- function(xdist,members=NULL){
-  if(is.null){
-   xdist2 <- xdist  
+  if(is.matrix(xdist)){
+    mdist <- xdist
+    xdist <- as.dist(xdist)
+  } else {
+    mdist <- as.matrix(xdist)
   }
-  xdist2 <- xdist[rownames(xdist)%in%members,colnames(xdist)%in%members,drop=F]
+  if(is.null(members)){
+    xdist2 <- xdist  
+  } else {
+    xdist2 <- as.dist(mdist[attr(xdist,"Labels")%in%members,attr(xdist,"Labels")%in%members,drop=F])
+  }
   xhc <- hclust(xdist2)
-  return(xhc)
+  xtree <- cutree(xhc,2)
+  xtree <- lapply(1:2,function(i){
+    names(which(xtree==i))
+   })
+  xdists <- lapply(xtree,function(x){
+    mdist[rownames(mdist)%in%x,colnames(mdist)%in%x,drop=F]
+  })
+  return(list(xtree=xtree,xdists=xdists))
 }
 
-test1 <- cluster(xdist)
+system.time(rlt <- cluster(xdist))
+i <- which(sapply(rlt$xtree,length)==max(sapply(rlt$xtree,length)))
+system.time(rltsub <- cluster(rlt$xdists[[i]],members=rlt$xtree[[i]]))
