1d042796e6270b4788ea2866eac51936c71e5a69
進撃の雯雯老師 <askahu6@gmail.com>
2017-03-28 12:27:51 -0500

    Update hclust.R

--- hclust.R
+++ hclust.R
@@ -1,8 +1,16 @@
+
+rm(list=ls())
+
 #Server
 
 load('xdist.rda')
-cluster <- function(xdist,members){
+cluster <- function(xdist,members=NULL){
+  if(is.null){
+   xdist2 <- xdist  
+  }
   xdist2 <- xdist[rownames(xdist)%in%members,colnames(xdist)%in%members,drop=F]
   xhc <- hclust(xdist2)
   return(xhc)
 }
+
+test1 <- cluster(xdist)
