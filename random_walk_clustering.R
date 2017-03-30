4cb6f7d1706093d7248ad834814d9279c8e248f3
進撃の雯雯老師 <askahu6@gmail.com>
2017-03-30 16:44:12 -0500

    Update random_walk_clustering.R

--- random_walk_clustering.R
+++ random_walk_clustering.R
@@ -2,3 +2,13 @@
 rm(list=ls())
 setwd("/home/zhu/rushdata/expression_clustering")
 load('gene_feature.rda')
+
+cmax <- cor(t(x)); gc();
+dmax <- as.matrix(dist(x)); gc();
+
+library(igraph)
+g.cmax <- igraph::graph_from_adjacency_matrix(cmax>0.7,mode='undirected')
+g.dmax <- igraph::graph_from_adjacency_matrix(dmax<0.16,mode='undirected')
+
+system.time(rw.cmax <- cluster_walktrap(g.cmax))
+system.time(rw.dmax <- cluster_walktrap(g.dmax))
