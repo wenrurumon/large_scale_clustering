b173dacc004dd214fc488bfa5442f1f8bad9d564
進撃の雯雯老師 <askahu6@gmail.com>
2017-03-30 20:57:30 -0500

    Update methylation_clustering.R

--- methylation_clustering.R
+++ methylation_clustering.R
@@ -3,8 +3,13 @@ rm(list=ls())
 setwd('/home/zhu/rushdata/processed/methylation')
 f <- dir()
 data <- lapply(f,function(x){
+  print(x)
   load(x)
-  sapply(pmethy,function(x){
+  raw <- scale(sapply(pmethy,function(x){
    rowMeans(x$methy)
-  })
+  }))[,]
+  cmax <- cor(raw)
+  dmax <- as.matrix(dist(t(raw)))
+  list(raw=raw,cmax=cmax,dmax=dmax)                    
 })
+
