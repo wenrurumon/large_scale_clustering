0c336ef48d16baf1737e80e7f8c3d203811d177f
進撃の雯雯老師 <askahu6@gmail.com>
2017-03-31 15:56:05 -0500

    Create expression_clustering.R

---
+++ expression_clustering.R
@@ -0,0 +1,48 @@
+
+rm(list=ls())
+
+#Server
+
+load('x.rda')
+system.time(xdist <- 1-cor(t(x)))
+
+cluster <- function(xdist,members=NULL,speed=2){
+  if(is.matrix(xdist)){
+    mdist <- xdist
+    xdist <- as.dist(xdist)
+  } else {
+    mdist <- as.matrix(xdist)
+  }
+  if(is.null(members)){
+    xdist2 <- xdist  
+  } else {
+    xdist2 <- as.dist(mdist[attr(xdist,"Labels")%in%members,attr(xdist,"Labels")%in%members,drop=F])
+  }
+  xhc <- hclust(xdist2)
+  xtree <- cutree(xhc,speed)
+  xtree <- lapply(1:speed,function(i){
+    names(which(xtree==i))
+   })
+  xdists <- lapply(xtree,function(x){
+    mdist[rownames(mdist)%in%x,colnames(mdist)%in%x,drop=F]
+  })
+  return(list(xtree=xtree,xdists=xdists))
+}
+
+cluster2 <- function(xdist){
+  if(is.matrix(xdist)){
+    rlt <- cluster(xdist)
+  }else{
+    rlt <- xdist
+  }  
+  while(max(sapply(rlt$xtree,length))>200){
+    i <- which(sapply(rlt$xtree,length)==max(sapply(rlt$xtree,length)))[1]
+    system.time(rltsub <- cluster(rlt$xdists[[i]],members=rlt$xtree[[i]]))
+    rlt$xtree <- c(rlt$xtree[-i],rltsub$xtree)
+    rlt$xdists <- c(rlt$xdists[-i],rltsub$xdists)
+    print(sapply(rlt$xtree,length))
+  }
+  return(rlt)
+ }
+ 
+ rlt.clustering <- cluster2(xdist)
