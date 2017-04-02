4a745fa24ef3b3606768d4360b68d7d4d1ae6215
進撃の雯雯老師 <askahu6@gmail.com>
2017-04-02 11:26:02 -0500

    Update plot_clustering.R

--- plot_clustering.R
+++ plot_clustering.R
@@ -18,3 +18,25 @@ dat.m <- ddply(dat.m,.(case),mutate,score=scale(value))
 ggplot(data = dat.m, aes(x = case, y = city))+
   geom_tile(aes(fill = score),colour='white') +
   scale_fill_gradient(low = "white",high = "steelblue")
+
+##################
+
+rm(list=ls())
+dat <- USArrests
+
+ggheatmap <- function(dat,dimnames=NULL,col=c('white','white','steelblue')){
+  dat <- scale(as.matrix(dat))[,]
+  dat.m <- melt(dat,id=rownames(dat))
+  if(!is.null(dimnames)){
+    colnames(dat.m)[1:2] <- dimnames
+  } else {
+    colnames(dat.m)[1:2] <- c('id','variables')
+  }
+  s <- paste0('ggplot(data=dat.m,aes(x=',colnames(dat.m)[2],',y=',colnames(dat.m)[1],'))+',
+         'geom_tile(aes(fill=value),colour="',col[1],'")+',
+         'scale_fill_gradient(low="',col[2],'",high="',col[3],'")')
+  eval(parse(text=s))
+}
+
+ggheatmap(dat,col=c('gray','white','red'))
+
